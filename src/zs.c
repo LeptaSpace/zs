14b6d2f957b1ba223ae3ca9f00b9c77596a0ce38
Pieter Hintjens <ph@imatix.com>
2015-05-24 16:29:43 +0200

    Problem: can't edit in zs command line
    Solution: use GNU readline if/when available... build using an
    external script (mkshell).

--- src/zs.c
+++ src/zs.c
@@ -1,6 +1,9 @@
 //  This is a ZeroScript machine
 
 #include "zs_classes.h"
+#ifdef USE_READLINE
+#   include <readline/readline.h>
+#endif
 
 int main (int argc, char *argv [])
 {
@@ -19,6 +22,11 @@ int main (int argc, char *argv [])
     zs_repl_t *repl = zs_repl_new ();
     zs_repl_verbose (repl, verbose);
 
+#ifdef USE_READLINE
+    //  Disable default completion on filenames
+    rl_bind_key ('\t', rl_insert);
+#endif
+
     //  If run with arguments, treat as script to execute
     if (argn < argc) {
         char input [1024 + 2] = "";
@@ -32,14 +40,25 @@ int main (int argc, char *argv [])
     else {
         //  If run without arguments, drop into REPL shell
         while (!zctx_interrupted) {
-            printf (zs_repl_completed (repl)? "> ": ": ");
+            char *prompt = zs_repl_completed (repl)? "> ": ": ";
+#ifdef USE_READLINE
+            char *input = readline (prompt);
+            if (!input || streq (input, "quit"))
+                break;
+            add_history (input);
+#else
+            printf ("%s", prompt);
             char input [1024 + 2];          //  1024 chars + LF + null
             if (!fgets (input, 1026, stdin) || streq (input, "quit\n"))
                 break;
+#endif
             if (zs_repl_execute (repl, input)) {
                 printf ("  %*c\n", zs_repl_offset (repl), '^');
                 puts ("Syntax error");
             }
+#ifdef USE_READLINE
+            free (input);
+#endif
         }
     }
     zs_repl_destroy (&repl);
